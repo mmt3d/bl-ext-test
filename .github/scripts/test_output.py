aae4dfd1f676e8715700ba38cad10f3b4a219d50
mmt3d <78088314+mmt3d@users.noreply.github.com>
2026-06-30 11:15:04 +0900

    test

--- .github/scripts/test_output.py
+++ .github/scripts/test_output.py
@@ -6,6 +6,9 @@ def main():
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         print(f"zip_name={zip_name}", file=fh)
 
+    toml_path = 'test-ver.toml'
+    print(f"マニフェスト生成完了: {toml_path}")
+
 
 if __name__ == "__main__":
     main()

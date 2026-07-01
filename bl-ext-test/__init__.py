c421a5a1f684bb06ca46c9a18ca1b05140a1ee59
mmt3d <78088314+mmt3d@users.noreply.github.com>
2026-07-02 05:40:22 +0900

    test

--- bl-ext-test/__init__.py
+++ bl-ext-test/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     'name': "Blender Extension Test",
     'author': 'mmt3d',
-    'version': (2026, 7, 6),
+    'version': (2026, 7, 7),
     'blender': (3, 3, 0),
     'location': "location test",
     'description': "description test",
